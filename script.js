2fa09a3b34085501f862ed9e28acf5dab9fa43f7
Safiya@0619 <saficute05@gmail.com>
2023-08-16 12:06:34 +0530

    Project-3 is here

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ fetch(URL)
   .then((res) => res.json())
   .then((res) => {
     console.log(res);
-    const divElement = document.createElement("div");
+
     const x = res.map((data) => {
       return `<div>
-      Name: ${data.name}<br>
+      <b>Name: ${data.name}</b><br>
       Email:${data.email}<br>
       Street:${data.address.street}<br>
       City:${data.address.city}
       </div>`;
     });
-    divElement.innerHTML = x.join("");
-    document.getElementById("dataContainer").appendChild(divElement);
+
+    document.getElementById("dataContainer").innerHTML = x.join("");
   });
